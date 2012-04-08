5ad0c7d27ec2715d3ef8e77c3ea5de39afe5bd3a
Nishant <nishant.vijayakumar@gmail.com>
2012-04-08 00:26:01 -0400

    added flickr/genRDS

---
+++ thesis/generateRDS.py
@@ -0,0 +1,44 @@
+"""
+Generates an RW crawl.
+
+"""
+import flickr
+import random
+
+def main():
+    MAXHOPS = 20 #number of hops
+    rdsConst = 0.3 #30% probability of activation of node 
+    userID = "25833004@N07"#zabong
+    randomWalk(userID, MAXHOPS)
+
+def generateRDS(MAXHOPS, rdsConst):
+    pass
+
+def randomWalk(userID, MAXHOPS):
+    nodeList = []
+    nodeSet = set()
+    numRounds = 0
+    for i in range(MAXHOPS):
+        nodeList.append(userID)
+        nodeSet.add(userID)
+        userList = flickr.contacts_getPublicList(user_id=userID)
+        if(userList):
+            randID = random.randint(1, len(userList))   #get a random number 
+            user = userList[randID-1]       #get the user indexed by that number
+            try:
+                userID = str(user.id)
+                print randID, userID
+            except:
+                pass
+        else: 
+            pass
+            #fix for checking local minima...
+          
+        if ( len(nodeList) - len(nodeSet) > int(MAXHOPS * 0.1) ):
+            print ("nodeList: %s, nodeSet: %s, returning..."%(len(nodeList), len(nodeSet)))
+            print nodeList
+            return
+    print nodeList        
+
+if __name__ == "__main__":
+    main()
