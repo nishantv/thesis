801d5b3e9eea969a6022dd737524b4fd200d8c76
Nishant <nishant.vijayakumar@gmail.com>
2012-04-11 23:28:04 -0400

    adds visited adj lists to hashtable

--- thesis/generateRW.py
+++ thesis/generateRW.py
@@ -6,7 +6,7 @@ import flickr
 import random
 
 def main():
-    MAXHOPS = 20 #number of hops    
+    MAXHOPS = 200 #number of hops    
     startRW(MAXHOPS)
     startRDS(MAXHOPS)
 
@@ -22,25 +22,42 @@ def startRW(MAXHOPS):
     for line in fp: 
         userID = line.rstrip("\n")
         print ("Current userID: %s"%userID)
+        ##userList = flickr.contacts_getPublicList(user_id=userID)
+        ##if userList: print type(userList), userList, len(userList)
+        ##else: print "no userList for %s"%userID
         randomWalk(userID, MAXHOPS)    
-    #userID = "25833004@N07"#zabong
+        #userID = "25833004@N07"#zabong
     
 def randomWalk(userID, MAXHOPS):
+    nodeDict = {}
     nodeList = []
     nodeSet = set()
     numRounds = 0
     for i in range(MAXHOPS):
+        #print nodeDict
         nodeList.append(userID)
         nodeSet.add(userID)
-        userList = flickr.contacts_getPublicList(user_id=userID)
-        if(userList):
-            randID = random.randint(1, len(userList))   #get a random number 
+        if userID in nodeDict:
+            userList = nodeDict[userID] 
+        else:
+            userList = flickr.contacts_getPublicList(user_id=userID)
+        if(isinstance(userList, list)): #making sure the node has contacts
+            #print type(userList), userList, len(userList)
+            #newList = [str(userID.id) for userID in userList]
+            #newList = userList
+            #print newList
+            nodeDict[userID] = userList
+            randID = random.randint(0, len(userList))   #get a random number 
             user = userList[randID-1]       #get the user indexed by that number
+            userID = str(user.id)
+            print "  -%s"%userID
+            """
             try:
                 userID = str(user.id)
                 print randID, userID
             except:
                 pass
+            """
         else: 
             pass
 
@@ -49,7 +66,7 @@ def randomWalk(userID, MAXHOPS):
             print ("nodeList: %s, nodeSet: %s, returning..."%(len(nodeList), len(nodeSet)))
             print nodeList
             return
-    print nodeList        
+    print nodeList#, nodeDict        
 
 if __name__ == "__main__":
     main()
